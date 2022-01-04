cabf5832dbe5ee6840ab746e7efb37f37e0bad70
SP <sarv.dgl@gmail.com>
2022-01-04 00:53:02 -0600

    Updated with SCM and other details

---
+++ src/main/java/com/example/repo/SpringRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepoApplication.class, args);
+	}
+
+}
